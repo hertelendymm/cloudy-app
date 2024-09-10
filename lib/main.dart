448dca6e97b986828ae876b84bbcc710aa290bfe
hertelendymm <hertelendymartonm@gmail.com>
2024-09-11 00:59:39 +0200

    Small updates

--- lib/main.dart
+++ lib/main.dart
@@ -34,23 +34,200 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var weatherData;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
+    // _isLoading = true;
     getLocationData();
   }
 
   void getLocationData() async {
     weatherData = await WeatherModel().getLocationWeather();
+    print("$weatherData=====================");
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    return _isLoading ? _loadingPage() : _currentLocation();
+    // return Scaffold(
+    //   body: SafeArea(child: _isLoading ?
+    //   const CircularProgressIndicator():
+    //   Container(child: Text(weatherData.toString()),),
+    //   ),
+    // );
+  }
+
+  Widget _loadingPage(){
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget _currentLocation(){
     return Scaffold(
-      body: SafeArea(child: weatherData != null ?
-      Container() :
-      const CircularProgressIndicator(),
+      // backgroundColor: isNight ? Color(0xFF333B49) : Colors.white,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      // backgroundColor: isNight ? Colors.black : Colors.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        // backgroundColor: isNight ? Color(0xFF333B49) : Colors.white,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        // backgroundColor: isNight ? Colors.black : Colors.white,
+        leading: GestureDetector(
+          onTap: () async {
+            var weatherData = await weather.getLocationWeather();
+            updateUI(weatherData);
+          },
+          child: Padding(
+            padding:
+            const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Icon(
+              FontAwesomeIcons.syncAlt,
+              color: Theme.of(context).iconTheme.color,
+              // color: isNight ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+        title: Center(
+          child: Text(
+            'Cloudy App',
+            style: kCityTitleTextStyleNight.copyWith(
+              fontSize: 20.0,
+              color: Theme.of(context).iconTheme.color,
+              // color: isNight ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () async {
+              var weatherData = await weather.getLocationWeather();
+              updateUI(weatherData);
+              build(context);
+            },
+            child: ChangeThemeButtonWidget(),
+          ),
+          // showThemeChangerButton(),
+          GestureDetector(
+            onTap: () async {
+              var typedName = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return CityScreen();
+                    // return CityScreen(isNight: isNight);
+                  },
+                ),
+              );
+              if (typedName != null) {
+                var weatherData = await weather.getCityWeather(typedName);
+                updateUI(weatherData);
+              }
+            },
+            child: Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Icon(
+                FontAwesomeIcons.searchLocation,
+                color: Theme.of(context).iconTheme.color,
+                // color: isNight ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Current weather:',
+                        textAlign: TextAlign.center,
+                        style: kCityTextStyle.copyWith(
+                          fontSize: 16.0,
+                          color: Theme.of(context).iconTheme.color,
+                          // color: isNight ? Colors.white : Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '${description.toUpperCase()}',
+                        textAlign: TextAlign.center,
+                        style: kCityTextStyle.copyWith(
+                          fontSize: 20.0,
+                          color: Theme.of(context).iconTheme.color,
+                          // color: isNight ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    weatherIcon.iconData,
+                    color: weatherIcon.color,
+                    size: MediaQuery.of(context).size.width / 3,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.thermometerHalf,
+                            color: Theme.of(context).iconTheme.color,
+                            // color: isNight ? Colors.white : Colors.black,
+                          ),
+                          SizedBox(width: 10.0),
+                          Text(
+                            '$temperature°C',
+                            style: kTempTextStyle.copyWith(
+                              fontSize: 25.0,
+                              color: Theme.of(context).iconTheme.color,
+                              // color: isNight ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        '$cityName',
+                        style: kTempTextStyle.copyWith(
+                          fontSize: 16.0,
+                          color: Theme.of(context).iconTheme.color,
+                          // color: isNight ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ButtonRounded(
+                    isNightMode: themeProvider.isDarkMode,
+                    function: () {
+                      showInterstitialAd();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ForecastScreen(
+                              lat: lat,
+                              lon: lon,
+                              // isNight: isNight,
+                            )),
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/button_rounded.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ButtonRounded extends StatelessWidget {
+  ButtonRounded({
+    required this.isNightMode,
+    required this.function,
+    required this.text,
+    this.isActive = false,
+  });
+
+  final bool isNightMode;
+  final Function function;
+  final String text;
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()=> function,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.1,
+        decoration: BoxDecoration(
+            color: isNightMode
+                ? Colors.white.withOpacity(0.1)
+                : Colors.black.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(10.0),
+            border: isActive
+                ? Border.all(
+              // width: isActive ? 5.0 : 0.0,
+              width: 5.0,
+              color: isNightMode
+                  ? Colors.white.withOpacity(0.2)
+                  : Colors.black.withOpacity(0.2),
+            )
+                : Border.all(
+              width: 0.0,
+              color: isNightMode
+                  ? Colors.white.withOpacity(0.1)
+                  : Colors.black.withOpacity(0.1),
+            )),
+        child: Center(
+          child: Text(text,
+              style: TextStyle(
+                color: isNightMode ? Colors.white : Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+      ),
+    );
+  }
+}
